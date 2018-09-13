2cbbf2af1479970fa65010529733b05a7c186e8b
xiaomiao <xiaomiao@example.com>
2018-09-13 16:50:23 +0800

    init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+import logo from './logo.svg'
+import './App.css'
+export default class App extends Component{
+  render(){
+    return(
+      <div>
+        <img src={logo} alt="logo" className='logo'/>
+        <h2 className='title'>App</h2>
+      </div>
+    )
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,7 @@
+//人口
+
+import React from 'react'
+import {render} from 'react-dom'
+import App from './App'
+
+render(<App/>,document.getElementById('root'))
